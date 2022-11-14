9a2a6749bde233e6636cd521b4fca635aa645d97
Aziz-84 <azca0001@gmail.com>
2022-11-14 16:12:29 +0100

    add function to create divs and class with mouse over event

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function gridtwoBytwo() {
-  for (let i = 0; i < 64; i++) {
-    for (let j = 0; j < 64; j++) {
-      const container = document.querySelector(".container");
+function grid16() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const container = document.querySelector(".container16");
       const cell = document.createElement("div");
       cell.className = "square";
       container.appendChild(cell);
@@ -11,4 +11,19 @@ function gridtwoBytwo() {
     }
   }
 }
-gridtwoBytwo();
+
+function grid32() {
+  for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < 32; j++) {
+      const container32 = document.querySelector(".container32");
+      const cell = document.createElement("div");
+      cell.className = "square32";
+      container32.appendChild(cell);
+      cell.addEventListener("mouseover", () => {
+        cell.setAttribute("style", "background-color:black");
+      });
+    }
+  }
+}
+
+grid32();
